6bc2711a1da99872852df5fd7dbe11b22a05fe2a
Ezeki Zibzibadze <ez@jok.io>
2022-09-05 13:12:01 +0400

    fix: generation logic

--- gen.ts
+++ gen.ts
@@ -3,8 +3,8 @@ import gen from './mod.ts'
 
 const flags = parseFlags(Deno.args)
 
-const endpointUrl = flags._[0] ?? flags.e
-const outputUrl = flags._[1] ?? flags.o ?? 'graphql.generated.ts'
+const endpointUrl = flags.e ?? flags._[0]
+const outputUrl = flags.o ?? 'graphql.generated.ts'
 const typeNamePrefix = flags.prefix ?? ''
 const enumToUnion = !flags.generateEnums
 
